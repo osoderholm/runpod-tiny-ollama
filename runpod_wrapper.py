cdd5483ac41e0df4611919bce1f3129ec13abaa9
Otto Söderholm <otto.soderholm@iki.fi>
2024-02-14 17:22:48 +0100

    Simplified too much, revert a bit

--- runpod_wrapper.py
+++ runpod_wrapper.py
@@ -15,16 +15,10 @@ class HandlerJob(TypedDict):
 def handler(job: HandlerJob):
     ollama_url = "http://0.0.0.0:11434"
     model = sys.argv[1]
-    system = job["system"]
+    input = job["input"]
 
-    input = {
-        "stream": False,
-        "model": model,
-        "prompt": job["prompt"],
-    }
-
-    if system:
-        input["system"] = system
+    input["stream"] = False
+    input["model"] = model
 
     response = requests.post(
         url=f"{ollama_url}/api/generate/",
